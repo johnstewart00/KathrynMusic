93403c12c22c3192a9e1a2152e69b6d62f76c218
johnstewart00 <stew.john560@gmail.com>
2024-11-25 22:56:51 -0700

    update for mobile spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,15 @@ export default function Home() {
         <div className="bg-gray-100 ml-10 my-16 p-10 rounded-md">
           <div className="md:flex md:justify-between items-center">
             {/* Left: Name and tagline */}
-            <div className="flex-1 pr-10">
-              <div className="text-9xl">Kathryn Stewart</div>
+            <div className="md:flex-1 pr-10">
+              <div className="md:text-9xl text-5xl">Kathryn Stewart</div>
               <div className="text-lg my-9">
                 Life is better with an appreciation for the arts
               </div>
             </div>
 
             {/* Right: Quote */}
-            <div className="bg-white p-4 shadow-md rounded-lg w-1/3 text-right">
+            <div className="bg-white p-4 shadow-md rounded-lg md:w-1/3 w-max text-center">
               <div className="text-sm italic">
                 “A piano is a beautiful mechanism, but it’s also a magical one.”
               </div>
