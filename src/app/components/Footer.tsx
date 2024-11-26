7d2ddf8a6aacbd4486c84583153f31eff81f3a9f
johnstewart00 <stew.john560@gmail.com>
2024-11-26 12:40:12 -0700

    try add sticky footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 md:p-10 mt:2">
+    <footer className="bg-gray-100 sticky bottom-0 mt-3 md:p-8">
       <div className="p-2 text-center">
         Contact Kathryn Gilbert at 208-881-2528
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Header />
 
       {/* Main Content */}
-      <div className="flex-grow overflow-y-auto">
+      <div className="flex-grow">
         <div className="bg-gray-100 md:ml-10 mx-2 m-8 md:my-16 p-5 md:p-10 rounded-md">
           <div className="md:flex md:justify-between items-center">
             {/* Left: Name and tagline */}
